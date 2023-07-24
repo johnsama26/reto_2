c82c19b7d79f37b8c2355651d41188c39eb06a32
johnsama26 <137846662+johnsama26@users.noreply.github.com>
2023-07-23 21:10:42 -0500

    prefabs

---
+++ Assets/script/Trampoline.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    public Animation animator;
+    public float jumpForce = 10f; // Fuerza del salto del trampolín
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = (Vector2.up * jumpForce);
+            animator.Play("JumpTrampoline");
+        }
+    }
+}
